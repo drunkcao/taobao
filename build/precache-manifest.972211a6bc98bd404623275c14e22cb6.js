14f711659aa716a8245d42c9cf14689781a31163
drunkcao <2570343007@qq.com>
2020-11-09 20:39:12 +0800

    update

---
+++ build/precache-manifest.972211a6bc98bd404623275c14e22cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0d13ee42cf5aebdc56d8fb153aea44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf29f7d3fed24f25ea11",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "d14c73cafcf55e21e74f",
+    "url": "/static/css/main.348fe9a8.chunk.css"
+  },
+  {
+    "revision": "bf29f7d3fed24f25ea11",
+    "url": "/static/js/2.6394d923.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.6394d923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14c73cafcf55e21e74f",
+    "url": "/static/js/main.a6fccda0.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
